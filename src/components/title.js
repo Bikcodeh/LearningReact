ffa40f1dfb8a9b62f63aa270152fd37583246434
Victor Hoyos <victor.micalisoft@gmail.com>
2019-07-26 15:45:11 -0500

    Primer componente basico

---
+++ src/components/title.js
@@ -0,0 +1,29 @@
+import React from 'react'
+
+/** Cuando le damos export default es porque estamos exportando la clase como tal  */
+
+/** Para que una clase se convierta en una clase que genere un componente  de react,
+ * debe de heredar de React.Component
+ */
+export default class Title extends React.Component{
+    /** La segunda caracteristica es que el component debe implementar el render y que ese elemento
+     * render retorne al menos un elemento html y no mas de un elemento html */
+    /** <p></p>
+     *  <p></p>  retonar eso no se puede, ya que hay mas de un elemento padre
+     * 
+     * <p>
+     *      <h2> </h2>
+     *      <h2> </h2>
+     * </p>
+     * Ese si se puede ya que es un solo elemento padre y puede tener muchos hijos.
+     */
+
+     render(){
+         return(
+             <div>
+                <h1>Places</h1>
+                <p>Descubre lugares de manera simple</p>
+             </div>
+         );
+     }
+}
