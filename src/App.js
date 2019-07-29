391069dc7316384a5a943e21d682f1452e083a73
Victor Hoyos <victor.micalisoft@gmail.com>
2019-07-29 09:14:23 -0500

    Aplicar estilos

--- src/App.js
+++ src/App.js
@@ -1,63 +1,61 @@
-import React from 'react';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 /** Como estamos importando el elemento por default, podemos darle un nombre al tag que usaremos para invocarlo */
-import Title from './components/title';
+import Title from "./components/title";
 
 /** cuando importamos un elemento que no es por default, tenemos que importalo con el nombre de el */
 
 /** Para manejar el state debemos agregar un valor por defecto en el constructor */
-class App extends React.Component{
-
-  constructor(props){
+class App extends React.Component {
+  constructor(props) {
     super(props);
 
-    /**  el this.state guarda datos de todo aquello que se pueden actualizar mientras se esta ejecutando el componente */
-    this.state = {
-      numero: 0
-    }
-    /**Y esto va en el constructor */
-    /**En vez de colocar la funcion directamente en el JSX, que colocando la funciona anonima etc, hay una forma mas elegante de pasar la funcion */
-    this.updateNumero = this.updateNumero.bind(this);
   }
 
-  updateNumero(){
-    this.setState({
-      numero: this.state.numero + 1
-    });
-  }
-
-  render (){
+  render() {
     return (
       <section>
-      <div>
-        <div> 
-          <Title></Title>
-          <h2> {this.state.numero} </h2>
-          { /** onClick es reservado de react */}
-          {/** Debemos procurar siempre usar arrow funtions para no corromper el ambito de react de los elementos */}
-          <button onClick={ this.updateNumero }>Crear una cuenta gratuita</button>
-          <img src={ process.env.PUBLIC_URL + 'images/icono01.png'} alt="icono bici"/>
-          <div>
-            <ul>
-              <li>
-                <h3>Calificaciones con emociones</h3>
-                <p>Califica tus lugares con experiencias, no con numeros</p>
-              </li>
-              <li>
-                <h3>Sin internet? Sin problemas</h3>
-                <p>Places funciona sin internet o en conexiones lentas</p>
-              </li>
-              <li>
-                <h3>Tus lugares favoritos</h3>
-                <p>Define tus lista de lugares favoritos</p>
-              </li>
-            </ul>
+        <div className="Header-background">
+          {/**Para aplicar estilos, primer forma */}
+          {/** <div style={{ "width" : "80%", "margin" : "0 auto"} }}></div> */}
+
+          {/** La otra forma es con la palabara reservada className */}
+          <div style={{ width: "80%", margin: "0 auto" }}>
+            <div className="Header-main">
+              <Title />
+              {/** onClick es reservado de react */}
+              {/** Debemos procurar siempre usar arrow funtions para no corromper el ambito de react de los elementos */}
+              <button>
+                Crear una cuenta gratuita
+              </button>
+              <img
+                src={process.env.PUBLIC_URL + "images/icono01.png"}
+                alt="icono bici"
+                className="Header-illustration"
+              />
+            </div>
+
+            <div>
+              <ul>
+                <li>
+                  <h3>Calificaciones con emociones</h3>
+                  <p>Califica tus lugares con experiencias, no con numeros</p>
+                </li>
+                <li>
+                  <h3>Sin internet? Sin problemas</h3>
+                  <p>Places funciona sin internet o en conexiones lentas</p>
+                </li>
+                <li>
+                  <h3>Tus lugares favoritos</h3>
+                  <p>Define tus lista de lugares favoritos</p>
+                </li>
+              </ul>
+            </div>
           </div>
         </div>
-      </div>
-    </section>
-    ) 
+      </section>
+    );
   }
 }
 
