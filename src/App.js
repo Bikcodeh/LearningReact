d6b7b3895156d5d33a80a3bca6b82e3a5581a48e
Victor Hoyos <victor.micalisoft@gmail.com>
2019-07-29 14:55:26 -0500

    Cards

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
 import React from "react";
 import Button from "@material-ui/core/Button";
 import purple from "@material-ui/core/colors/purple";
+import red from "@material-ui/core/colors/red";
+import { Card } from "@material-ui/core";
+import { CardContent } from "@material-ui/core";
+import Box from "@material-ui/core/Box";
 
 import "./App.css";
 
@@ -24,6 +28,7 @@ class App extends React.Component {
               <Title />
               {/** onClick es reservado de react */}
               {/** Debemos procurar siempre usar arrow funtions para no corromper el ambito de react de los elementos */}
+
               <Button variant="contained" color="secondary">
                 Crear una cuenta gratuita
               </Button>
@@ -35,20 +40,50 @@ class App extends React.Component {
             </div>
           </div>
         </div>
-        <div style={{ "background": purple[500], "padding" : "50px" }}>
+        <div style={{ background: purple[500], padding: "50px" }}>
           <ul>
-            <li>
-              <h3>Calificaciones con emociones</h3>
-              <p>Califica tus lugares con experiencias, no con numeros</p>
-            </li>
-            <li>
-              <h3>Sin internet? Sin problemas</h3>
-              <p>Places funciona sin internet o en conexiones lentas</p>
-            </li>
-            <li>
-              <h3>Tus lugares favoritos</h3>
-              <p>Define tus lista de lugares favoritos</p>
-            </li>
+            <Box mb={3}>
+              <Card className="Header-benefit">
+                <CardContent style={{ background: red[400] }}>
+                  <img
+                    src={process.env.PUBLIC_URL + "images/icono02.png"}
+                    alt="imagen"
+                    width="100px"
+                    height="100px"
+                  />
+                  <h3>Calificaciones con emociones</h3>
+                  <p>Califica tus lugares con experiencias, no con numeros</p>
+                </CardContent>
+              </Card>
+            </Box>
+            <Box mb={2}>
+              <Card className="Header-benefit">
+                <CardContent style={{ background: red[400] }}>
+                  <img
+                    src={process.env.PUBLIC_URL + "images/icono03.png"}
+                    alt="imagen"
+                    width="100px"
+                    height="100px"
+                  />
+                  <h3>Sin internet? Sin problemas</h3>
+                  <p>Places funciona sin internet o en conexiones lentas</p>
+                </CardContent>
+              </Card>
+            </Box>
+            <Box mb={2}>
+              <Card className="Header-benefit">
+                <CardContent style={{ background: red[400] }}>
+                  <img
+                    src={process.env.PUBLIC_URL + "images/icono01.png"}
+                    alt="imagen"
+                    width="100px"
+                    height="100px"
+                  />
+                  <h3>Tus lugares favoritos</h3>
+                  <p>Define tus lista de lugares favoritos</p>
+                </CardContent>
+              </Card>
+            </Box>
           </ul>
         </div>
       </section>
